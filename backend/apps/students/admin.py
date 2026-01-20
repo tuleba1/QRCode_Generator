b1dda69636103d59e83a0c8824dad62063545151
Túlio Dantas <sirtulio10@gmail.com>
2026-01-20 03:16:37 -0300

    feature: adição de SuperUser e estudante

--- backend/apps/students/admin.py
+++ backend/apps/students/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Student 
 # Register your models here.
+
+@admin.register(Student)
+
+# Administraçõa personalizada para o modelo Student
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("name", "registration", "total_presences", "created_at", "update_at")
+
+    search_fields = ("name", "registration")
